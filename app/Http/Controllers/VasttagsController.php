5d2b417d15cc8a8ad67ff3c6d93c6c7b0f43730e
Ertugrul2070 <e.karaduman2070@gmail.com>
2019-04-05 14:33:47 +0200

    vasttags overview done

--- app/Http/Controllers/VasttagsController.php
+++ app/Http/Controllers/VasttagsController.php
@@ -2,9 +2,96 @@
 
 namespace App\Http\Controllers;
 
+use App\Vasttag;
+use App\Zone;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class VasttagsController extends Controller
 {
-    //
+    /**
+     * Get all vasttags data
+     */
+    public function getVasttags()
+    {
+        $vasttags = Vasttag::all()->where('website.deleted_at', '=', null);
+        return view('admin.vasttags.overview', compact('vasttags'));
+    }
+
+    /**
+     * Get all vasttags data with the same ID as the variable
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getVasttag($id)
+    {
+        $vasttag = Vasttag::find($id);
+
+        return view('admin.vasttags.view', compact('vasttag'));
+    }
+
+    /**
+     * Collecting all User data and sending it to the create page
+     */
+    public function storePageVasttags()
+    {
+        $zones = Zone::all()->where('deleted_at', '=', null);
+        return view('admin.vasttags.create', compact('zones'));
+    }
+
+    /**
+     * Store a newly created recourse in database
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeVasttags(Request $request)
+    {
+        $requestData = $request->except('_token', 'create');
+        Vasttag::insert($requestData);
+
+        return redirect()->route('vasttags')->with('message', 'Succesfully saved');
+    }
+
+    /**
+     * Get all vasttags data with the same ID as the variable. And get all users
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updatePageVasttags($id)
+    {
+        $vasttags = Vasttag::find($id);
+
+        $zones = Vasttag::all()->where('deleted_at', '=', null);
+
+        return view('admin.vasttags.update', compact('vasttags', 'zones'));
+    }
+
+    /**
+     * Update an existing recourse in database
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function updateVasttags($id, Request $request)
+    {
+        $requestData = $request->except('_method','_token', 'update');
+        Vasttag::whereId($id)->update($requestData);
+
+        return redirect()->route('vasttags')->with('message', 'Succesfully saved');
+    }
+
+    /**
+     * Delete all recourse data with the same ID as the variable.
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteVasttags($id)
+    {
+        $vastId = Vasttag::find($id);
+
+        Vasttag::whereId($zoneId->website_id)->update(['deleted_at' => Carbon::now()]);
+
+        return redirect()->route('vasttags')->with('message', 'Succesfully deleted');
+    }
 }
--- resources/views/admin/vasttags/overview.blade.php
+++ resources/views/admin/vasttags/overview.blade.php
@@ -8,7 +8,7 @@
     <a href="{{url('/')}}">
         <button class="btn btn-success">Back</button>
     </a>
-    <a href="{{url('websites/createPage')}}">
+    <a href="{{url('vasttags/createPage')}}">
         <button class="btn btn-primary">Create</button>
     </a>
 </div>
@@ -16,54 +16,49 @@
     <thead>
     <tr>
         <th scope="col">#</th>
-        <th scope="col">name</th>
+        <th scope="col">provider name</th>
         <th scope="col">url</th>
-        <th scope="col">User</th>
-        <th scope="col">active</th>
+        <th scope="col">zone</th>
         <th scope="col">View</th>
         <th scope="col">Update</th>
         <th scope="col">Delete</th>
     </tr>
     </thead>
     <tbody>
+    <!--Start foreach loop-->
     <!--Retrive all the data and show data in table-->
-    @foreach($websites as $website)
+    @foreach($vasttags as $vasttag)
         <tr>
-            <th scope="row">{{$website->id}}</th>
-            <td>{{$website->name}}</td>
-            <td>{{$website->url}}</td>
-            <td>{{$website->user->name}}</td>
-            <!-- If the active value is 1 display "Yes". If its not display No-->
-            @if($website->active == 1)
-                <td>{{__('Yes')}}</td>
-                @else
-                <td>{{__('No')}}</td>
-                @endif
+            <th scope="row">{{$vasttag->id}}</th>
+            <td>{{$vasttag->provider_name}}</td>
+            <td>{{str_limit($vasttag->url, 50)}}</td>
+            <td>{{$vasttag->zone->div_tag}}</td>
             <td>
-                <a href="{{url('websites/'.$website->id.'/view')}}">
-                <button value="{{$website->id}}" class="btn btn-info">View</button>
+                <a href="{{url('vasttags/'.$vasttag->id.'/view')}}">
+                    <button value="{{$vasttag->id}}" class="btn btn-info">View</button>
                 </a>
             </td>
             <td>
-                <a href="{{url('websites/'.$website->id.'/updatePage')}}">
-                <button value="{{$website->id}}" class="btn btn-warning">Update</button>
+                <a href="{{url('vasttags/'.$vasttag->id.'/updatePage')}}">
+                    <button value="{{$vasttag->id}}" class="btn btn-warning">Update</button>
                 </a>
             </td>
             <td>
-                <a href="{{url('websites/'.$website->id.'/delete')}}">
-                <button value="{{$website->id}}" id="delete" name="{{$website->name}}" onclick="ConfirmDelete()" class="btn btn-danger">Delete</button>
+                <a href="{{url('vasttags/'.$vasttag->id.'/delete')}}">
+                    <button value="{{$vasttag->id}}" id="delete" name="{{$vasttag->name}}" onclick="ConfirmDelete()" class="btn btn-danger">Delete</button>
                 </a>
             </td>
         </tr>
     @endforeach
+    <!--End foreach loop-->
     </tbody>
 </table>
 
 <script type="text/javascript">
     function ConfirmDelete()
     {
-        var webName = document.getElementById('delete').name;
-        var x = confirm("Are you sure you want to delete " + webName+"?");
+        var vastName = document.getElementById('delete').name;
+        var x = confirm("Are you sure you want to delete " + vastName+"?");
         if (x)
             return true;
         else
